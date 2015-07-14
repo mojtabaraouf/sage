Fixed mistake in ram pressure stripping calculation.  More plots in discplots.

output/discplots.py
@@ -19,6 +19,9 @@ G = gr.sagesnap('model_z0.000', 0, 7, indir, disc=True) #Commit 4 - good results
 
 h = 0.678;
 
+angle_limit = 20 # Number of degrees between stellar and gas discs to use stellar info on HI/H2 ratio
+cos_angle_limit = np.cos(angle_limit*np.pi/180)
+
 DiscBinEdge = np.append(0, np.array([0.5*200*1.2**i for i in range(30)])) / h
 #DiscBinEdge = np.append(0, np.array([0.2*200*1.2**i for i in range(30)])) / h
 #DiscBinEdge = np.append(0, np.array([0.5*200*1.15**i for i in range(30)])) / h
@@ -54,7 +57,11 @@ for i in xrange(N):
     Sigma_gas = (G.DiscGas[filt][i]*1e10/h) / (np.pi*(radius_bins[1:]**2 - radius_bins[:-1]**2)*1e6)
     Sigma_gas_arr[i,:] = Sigma_gas
     #
-    H2_HI = 1.306e-3 * (Sigma_gas**2 + 0.1*Sigma_gas * np.sqrt(Sigma_star*Sigma_star[0]))**0.92
+    cos_angle = G.SpinGas[filt][i,0]*G.SpinStars[filt][i,0] + G.SpinGas[filt][i,1]*G.SpinStars[filt][i,1] + G.SpinGas[filt][i,2]*G.SpinStars[filt][i,2]
+    if cos_angle >= cos_angle_limit:
+        H2_HI = 1.306e-3 * (Sigma_gas**2 + 0.1*Sigma_gas * np.sqrt(Sigma_star*Sigma_star[0]))**0.92
+    else:
+        H2_HI = 1.306e-3 * Sigma_gas**1.84
     H2_gas = 0.75 * 1.0/(gc.divide(np.ones(len(H2_HI)),H2_HI) + 1) * (1 - gc.divide(G.DiscGasMetals[filt][i], G.DiscGas[filt][i]))
     #H2_gas = 1.0/(gc.divide(np.ones(len(H2_HI)),H2_HI) + 1)
     Sigma_H2_arr[i,:] = Sigma_gas_arr[i,:] * H2_gas
@@ -114,7 +121,7 @@ plt.ylabel(r'$\Sigma_{\rm star}$ [M$_{\bigodot}$ pc$^{-2}$]')
 plt.legend(fontsize=fsize-4, loc='best', frameon=False, title=r'$V_{\rm vir} \in$ [200,235] km s$^{-1}$')
 gp.Leroygals()
 gp.savepng(outdir+'StarSurface')
-
+"""
 gp.figure()
 plt.plot(rad_plot, Sigma_gas_av, 'r-', lw=2, label=r'Mean')
 plt.plot(rad_plot, Sigma_gas_med, 'r--', lw=2, label=r'Median')
@@ -129,7 +136,7 @@ plt.ylabel(r'$\Sigma_{\rm gas}$ [M$_{\bigodot}$ pc$^{-2}$]')
 plt.legend(fontsize=fsize-4, loc='best', frameon=False, title=r'$V_{\rm vir} \in$ [200,235] km s$^{-1}$')
 gp.Leroygals(HI=True, H2=True)
 gp.savepng(outdir+'GasSurface')
-
+"""
 gp.figure()
 plt.plot(rad_plot, Sigma_HI_av, 'r-', lw=2, label=r'Mean')
 plt.plot(rad_plot, Sigma_HI_med, 'r--', lw=2, label=r'Median')
@@ -194,7 +201,7 @@ plt.ylabel(r'$\Sigma_{\rm star}$ [M$_{\bigodot}$ pc$^{-2}$]')
 plt.legend(fontsize=fsize-4, loc='best', frameon=False, title=r'$V_{\rm vir} \in$ [175,200] km s$^{-1}$')
 gp.Leroygals(HighVvir=False, LowVvir=True)
 gp.savepng(outdir+'StarSurface2')
-
+"""
 gp.figure()
 plt.plot(rad_plot, Sigma_gas_av, 'r-', lw=2, label=r'Mean')
 plt.plot(rad_plot, Sigma_gas_med, 'r--', lw=2, label=r'Median')
@@ -208,7 +215,7 @@ plt.ylabel(r'$\Sigma_{\rm gas}$ [M$_{\bigodot}$ pc$^{-2}$]')
 plt.legend(fontsize=fsize-4, loc='best', frameon=False, title=r'$V_{\rm vir} \in$ [175,200] km s$^{-1}$')
 gp.Leroygals(HI=True, H2=True, HighVvir=False, LowVvir=True)
 gp.savepng(outdir+'GasSurface2')
-
+"""
 
 
 ### OTHER PLOTS FOR INTEGRATED PROPERTIES
@@ -238,7 +245,61 @@ gp.figure()
 gp.btf((G.StellarMass[filt]+G.ColdGas[filt])*1e10/h, G.Vmax[filt], h=h, fsize=fsize, extra=True, label=r'SAGE Disc')
 gp.savepng(outdir+'BTF', xpixplot=768, ypixplot=512)
 
+# Quiescent satellites
+filt = (G.Type!=0) * (G.StellarMass > 0.0)
+gp.figure()
+gp.quiescenthalo(G.StellarMass[filt]*1e10/h, (G.SfrBulge[filt]+G.SfrDisk[filt]), G.CentralMvir[filt]*1e10/h, h=h, Nbins=15, label=r'SAGE Disc', extra=True)
+gp.savepng(outdir+'QuiescentSat', xpixplot=768, ypixplot=512)
 
+# HI and H2 mass functions
+filt = (G.ColdGas > 0.0) * (G.Vvir > 0.0)
+Sigma_gas, Sigma_star = np.zeros((len(G[filt]), 30)), np.zeros((len(G[filt]), 30))
+DiscHI, DiscH2 = np.zeros((len(G[filt]), 30)), np.zeros((len(G[filt]), 30))
+cos_angle = G.SpinGas[filt][:,0]*G.SpinStars[filt][:,0] + G.SpinGas[filt][:,1]*G.SpinStars[filt][:,1] + G.SpinGas[filt][:,2]*G.SpinStars[filt][:,2]
+angle_filt = (cos_angle < cos_angle_limit)
+for i in xrange(30):
+    area = np.pi * (DiscBinEdge[i+1]**2 - DiscBinEdge[i]**2) * 1e6 / G.Vvir[filt]**2
+    Sigma_gas[:,i] = (G.DiscGas[filt][:,i]*1e10/h) / area
+    Sigma_star[:,i] = (G.DiscStars[filt][:,i]*1e10/h) / area
+    H2_HI = 1.306e-3 * (Sigma_gas[:,i]**2 + 0.1*Sigma_gas[:,i] * np.sqrt(Sigma_star[:,i]*Sigma_star[:,0]))**0.92
+    H2_HI[angle_filt] = 1.306e-3 * Sigma_gas[angle_filt][:,i]**1.84
+    DiscH2[:,i] = 0.75 * 1.0/(gc.divide(np.ones(len(H2_HI)),H2_HI) + 1) * (G.DiscGas[filt][:,i] - G.DiscGasMetals[filt][:,i]) * 1e10/h
+DiscHI = 0.75*G.DiscGas[filt]*1e10/h - G.DiscGasMetals[filt]*1e10/h - DiscH2
+
+#HImass = np.sum(DiscHI,axis=1)
+H2mass = np.sum(DiscH2,axis=1)
+HImass = 0.75*G.ColdGas[filt]*1e10/h - G.MetalsColdGas[filt]*1e10/h - H2mass
+
+gp.figure()
+gp.massfunction(HImass, 62.5/h, extra=0, h=h, step=False, fsize=fsize, binwidth=0.1, label=r'H\,\textsc{i} SAGE Disc')
+gp.massfunction(H2mass, 62.5/h, extra=0, h=h, step=False, fsize=fsize, binwidth=0.1, label=r'H$_2$ SAGE Disc', colour='c', ls='--')
+gp.massfunction_HI_H2_obs()
+plt.legend(fontsize=fsize-4, loc='best', frameon=False)
+gp.savepng(outdir+'GMFs', xpixplot=768, ypixplot=512)
+
+
+# Spread of angles between stellar and gas discs - Need to filter systems
+filt = (G.ColdGas>0.0) * (G.StellarMass - G.SecularBulgeMass - G.ClassicalBulgeMass > 0.0)
+cos_theta = G.SpinGas[filt][:,0]*G.SpinStars[filt][:,0] + G.SpinGas[filt][:,1]*G.SpinStars[filt][:,1] + G.SpinGas[filt][:,2]*G.SpinStars[filt][:,2]
+cos_theta[cos_theta>1.0] = 1.0
+cos_theta[cos_theta<-1.0] = -1.0
+theta = np.arccos(cos_theta)*180/np.pi
+gp.figure()
+plt.hist(theta, bins=30)
+plt.xlabel(r'Angle between gas and stellar discs [degrees]')
+plt.ylabel(r'Number')
+ymin, ymax = plt.gca().get_ylim()
+plt.text(100, 0.75*ymax-0.25*ymin, str(100.0*len(theta[theta<=10])/len(theta))[:4]+r' $\% \leq 10^{\circ}$' )
+gp.savepng(outdir+'SpinOffset', xpixplot=768, ypixplot=512)
+
+# Mass distributions for offset galaxies
+gp.figure()
+plt.hist(np.log10(G.ColdGas[filt][theta>20]*1e10/h), bins=30, color='b', label=r'Cold Gas', histtype='step')
+plt.hist(np.log10(G.StellarMass[filt][theta>20]*1e10/h), bins=30, color='r', label=r'Stars', histtype='step')
+plt.legend(fontsize=fsize-4, loc='best', frameon=False)
+plt.xlabel(r'$\log_{10}(M_{\rm stars}\ \mathrm{or}\ M_{\rm gas}\ [\mathrm{M}_{\bigodot}])$')
+plt.ylabel('Number of galaxies with offset $> 20^{\circ}$')
+gp.savepng(outdir+'MassDistOffset', xpixplot=768, ypixplot=512)
 
 # z=2 Stellar mass function
 G = gr.sagesnap('model_z2.239', 0, 7, indir, disc=True)
@@ -248,6 +309,8 @@ gp.smf_nifty_obs(h, z=2, haxes=False)
 plt.legend(fontsize=fsize-4, loc='best', frameon=False, title=r'$z=2$')
 plt.axis([8,11.8,5e-6,0.2])
 gp.savepng(outdir+'SMF_z2', xpixplot=768, ypixplot=512)
+
+
 
 ### BH mass history plot
 zstr = ['0.000', '0.089', '0.208', '0.362', '0.564', '0.828', '1.173', '1.630', '2.239', '3.060', '4.179', '5.724', '7.883', '10.944', '15.343']